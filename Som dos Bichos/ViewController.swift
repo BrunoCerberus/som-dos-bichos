85a193013fe53fd3a74da7f8db85fa65459e1a19
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-18 14:50:52 -0200

    primeiro e ultimo push

--- Som dos Bichos/ViewController.swift
+++ Som dos Bichos/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    private func executarSom(_ som: String) {
+        
+        if let path = Bundle.main.path(forResource: som, ofType: "mp3") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                player.play()
+                
+            } catch let erro {
+                print(erro.localizedDescription)
+            }
+        }
+       
+    }
+    
+    @IBAction func late(_ sender: Any) {
+        executarSom("cao")
+    }
+    
+    @IBAction func mia(_ sender: Any) {
+        executarSom("gato")
+    }
+    
+    @IBAction func rosna(_ sender: Any) {
+        executarSom("leao")
+    }
+    
+    @IBAction func macaquice(_ sender: Any) {
+        executarSom("macaco")
+    }
+    
+    @IBAction func baa(_ sender: Any) {
+        executarSom("ovelha")
+    }
+    
+    @IBAction func moo(_ sender: Any) {
+        executarSom("vaca")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
